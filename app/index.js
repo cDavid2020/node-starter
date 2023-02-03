4e3631bf6c201372a14d1a0c9b8c73b125fdc079
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-02-02 18:41:47 -0600

    Add 'http-module-nodecore' with notes

--- app/index.js
+++ app/index.js
@@ -1,1 +1,45 @@
 export const add2Nums = (num1, num2) => num1 + num2;
+
+
+// Node & Express js REST API
+
+// Request
+
+GET /api/customers
+
+// Response
+
+[
+  {id:1, name: 'John', age:}
+  {id:2, name: 'Bert', age:}
+  ...
+]
+
+// an array of customers👆
+
+// Request
+
+PUT /api/customers/1 // represents customer with completed properties
+
+{ name: 'Customer }
+
+
+
+//👇the server updates the customer in the Response object
+// Response
+
+{id:1, name: 'John', age:}
+
+// DELETE A CUSTOMER
+
+// Request
+POST / api/customers
+
+{ name: 'John', age:}
+
+// DELETE /api/customers/{id}
+// The delete part can be left blank unlike 👆
+
+
+
+// Response
---
+++ app/rest-operators.md
@@ -0,0 +1,11 @@
+/*
+These are REST operations
+
+GET /api/customers
+GET /api/customers/1
+PUT /api/customers/1
+DELETE /api/customers/1
+POST /api/customers/
+ */
+
+
